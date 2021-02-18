a71c4a7d332ef99b5178b981f117fa2edf87c270
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-02-18 06:34:46 +0100

    deleteroutes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
- resources :salons
+#  resources :salons
+ 
   resources :epilations do
     resources :order_items, only: [:create, :update, :destroy]
   end
