bd4bcc2001cdeb6a0cf1a3d57afbea9a7101f43c
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-02-19 17:05:34 +0100

    mapok+mapseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Salon.create(address: 'Résidence Saint-Georges, 13150 Tarascon', name:"Relook Esthétique", latitude: '43.80929033885456', longitude: '4.665644661374886')
