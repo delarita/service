4c83d1476d56fd66060a6108766977087f49e078
julien daniel <j.delarita@gmail.com>
2021-02-19 11:56:53 +0100

    debogage sur mapbox_rescue_encore_off

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,29 +18,29 @@ class ApplicationController < ActionController::Base
     #end
 
 
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  rescue_from Exception, with: :not_found
-  rescue_from ActionController::RoutingError, with: :not_found
+  # rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  # rescue_from Exception, with: :not_found
+  # rescue_from ActionController::RoutingError, with: :not_found
 
-  def raise_not_found
-    raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
-  end
+  # def raise_not_found
+  #   raise ActionController::RoutingError.new("No route matches #{params[:unmatched_route]}")
+  # end
 
-  def not_found
-    respond_to do |format|
-      format.html { render file: Rails.root.join('public', '404.html'), layout: false, status: :not_found }
-      format.xml { head :not_found }
-      format.any { head :not_found }
-    end
-  end
+  # def not_found
+  #   respond_to do |format|
+  #     format.html { render file: Rails.root.join('public', '404.html'), layout: false, status: :not_found }
+  #     format.xml { head :not_found }
+  #     format.any { head :not_found }
+  #   end
+  # end
 
-  def error
-    respond_to do |format|
-      format.html { render file: Rails.root.join('public', '500.html'), layout: false, status: :error }
-      format.xml { head :not_found }
-      format.any { head :not_found }
-    end
-  end
+  # def error
+  #   respond_to do |format|
+  #     format.html { render file: Rails.root.join('public', '500.html'), layout: false, status: :error }
+  #     format.xml { head :not_found }
+  #     format.any { head :not_found }
+  #   end
+  # end
 
   private
 
