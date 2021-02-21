96b2d3f2255dfa0b2289f947762749c8fe3f2c4d
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-02-21 18:21:52 +0100

    lastmodifs

--- app/controllers/salons_controller.rb
+++ app/controllers/salons_controller.rb
@@ -1,13 +1,16 @@
 class SalonsController < ApplicationController
-  def index
-    @salons = policy_scope(Salon)
+  skip_before_action :authenticate_user!, only: [ :index ]
 
-    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
-    @markers = @salons.geocoded.map do |salon|
-      {
-        lat: salon.latitude,
-        lng: salon.longitude
-      }
+    def index
+      @salons = policy_scope(Salon)
+
+      # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+      @markers = @salons.geocoded.map do |salon|
+        {
+          lat: salon.latitude,
+          lng: salon.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { salon: salon })
+        }
+        end
     end
-  end
 end
