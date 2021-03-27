3318cbdedf8f5f238cf1e05e4b55a86bdb9b50ca
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-03-27 18:33:35 +0100

    petitesmodifs

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
     skip_after_action :verify_authorized
-
+    skip_before_action :authenticate_user!
       def new
         
         @contact = Contact.new
