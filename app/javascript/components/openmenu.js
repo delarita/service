fecdcca2f16eda2938e26cc2450d59365ed2d5f8
delarita <j.delarita@gmail.com>
2021-04-12 10:17:00 +0200

    ponct trainante-bcbouton-endmanquant

--- app/javascript/components/openmenu.js
+++ app/javascript/components/openmenu.js
@@ -1,18 +1,16 @@
 const openNav = () => {
-   
-    
+
+
     const button = document.querySelector(".menu-button");
     const openDiv = document.querySelector(".menu-mobile")
     const menuDiv = document.querySelector(".navmenu2")
-  
+
     button.addEventListener("click", (event) => {
       console.log(event)
       openDiv.classList.toggle("active");
       menuDiv.classList.toggle("active");
       button.classList.toggle("colorize")
-      
-  });
-  
+    });
   };
-  
+
   export { openNav };
