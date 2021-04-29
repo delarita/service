54030f3fb03e3cbceba37d0f13810c8e7617b594
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-04-29 19:55:14 +0200

    blubbybutton

---
+++ app/javascript/components/blubby-button.js
@@ -0,0 +1,22 @@
+const animateButton = (e) => {
+if (e) {
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  let bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('mouseover', animateButton, false);
+  }
+
+
+}
+
+  export { animateButton };
