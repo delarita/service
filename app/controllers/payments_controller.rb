29a8606cea3bbe5213aa22841c2cc17a357cf4ba
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-05-08 20:43:55 +0200

    prestainmail

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,14 @@
 class PaymentsController < ApplicationController
   skip_after_action :verify_authorized
 
+
   def new
     @order = current_user.orders.where(state: 'pending').find(params[:order_id])
   end
+
+   
+
+ 
+  
+  
 end
